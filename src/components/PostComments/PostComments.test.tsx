5f680f15a6fab179bf0324e20df24e953a602828
GUILHERME BARBOSA DE LIMA <126630427+guin1@users.noreply.github.com>
2023-12-09 09:43:46 -0300

    atualizando teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,27 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+  test('Deve renderizar o componente corretamente', () => {
+    render(<PostComment />);
+    expect(screen.getByText('Comentar')).toBeInTheDocument();
+  });
+
+  test('Deve adicionar dois comentários corretamente', () => {
+    render(<Post />);
+      
+    // Procurar o textarea pelo data-testid
+    const textarea = screen.getByTestId('post-comments-form-textarea');
+
+    // Envio do primeiro comentário
+    fireEvent.change(textarea, { target: { value: 'Primeiro comentário' } });
+    fireEvent.submit(screen.getByTestId('post-comments-form'));
+
+    // Segundo comentário
+    fireEvent.change(textarea, { target: { value: 'Segundo comentário' } });
+    fireEvent.submit(screen.getByTestId('post-comments-form'));
+
+    // Chamar a renderização dos comentários
+    expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+    expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
+  });
 });
